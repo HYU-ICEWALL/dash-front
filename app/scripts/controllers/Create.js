9db3e35e32a0bb30e6cce01efa3d3972931d9a0d
Sanggyu Nam <pokeplus@gmail.com>
2013-09-10 01:46:34 +0900

    [ci skip] passing 2 of 3 tests for `CreateCtrl`
    * I added `$q` and `MajorInfo` as new dependencies for `CreateCtrl` controller.
    * I added a function that adds a class object to class cart of scope.
    * I added a function that converts the class cart of `storedContext` into the class cart to expose to scope.
    * I added a private variable `storedClassCart` to save the class cart data in form of the class cart of `storedContext`.
    * I added `search` function to `$scope` which searches the classes with keyword `$scope.keyword`.
    * I modified the test data for the unit test of `CreateCtrl` controller.
    * I fixed the injected services to work correctly.
    
        * Now the success callbacks in `Class` service are called asynchronously.
    
    * I changed the spec 'should store context data' to 'should delete a class entity from class cart and store the class cart into cookie'.
    
        * Now the spec has been changed to manipulate `scope.classCart` and `storedClassCart` directly rather than to `$watch(scope.classCart)`.

--- app/scripts/controllers/Create.js
+++ app/scripts/controllers/Create.js
@@ -1,15 +1,192 @@
 'use strict';
 
 angular.module('dashApp')
-.controller('CreateCtrl', ['$scope', 'storedContext', 'Class',
-function ($scope, storedContext, Class) {
-  var convertStoredClassCart = function (classCart) {
-    angular.forEach(classCart, function (entity) {
-      //
+.controller('CreateCtrl', ['$scope', '$q', 'storedContext', 'MajorInfo', 'Class',
+function ($scope, $q, storedContext, MajorInfo, Class) {
+  var addClassToClassCart = function (classObj, classCart) {
+    var course_no = classObj.course_no;
+    var major = classObj.major;
+    var hasFoundCartEntity = false;
+
+    var addToClasses = function (classObj, classes) {
+      var fixed;
+      if (typeof classObj.fixed !== 'undefined') {
+        fixed = classObj.fixed;
+      } else {
+        fixed = false;
+      }
+      var classEntity = {
+        instructor: classObj.instructor,
+        class_no: classObj.class_no,
+        fixed: fixed
+      };
+      classes.push(classEntity);
+    };
+
+    var addToMajors = function (classObj, majors) {
+      var majorEntity = {
+        code: major,
+        grade: classObj.grade,
+        credit: classObj.credit,
+        classes: []
+      };
+
+      MajorInfo.getNameByCode(major).then(function (name) {
+        majorEntity.name = name;
+      });
+
+      addToClasses(classObj, majorEntity.classes);
+
+      majors.push(majorEntity);
+    };
+
+    var addToCart = function (classObj, cart) {
+      var placement_required;
+      if (typeof classObj.placement_required !== 'undefined') {
+        placement_required = classObj.placement_required;
+      } else {
+        placement_required = isPlacementRequired(classObj);
+      }
+
+      var cartEntity = {
+        placement_required: placement_required,
+        course_no: course_no,
+        name: classObj.name,
+        majors: []
+      };
+
+      addToMajors(classObj, cartEntity.majors);
+      cart.push(cartEntity);
+    };
+
+    angular.forEach(classCart, function (cartEntity) {
+      if (cartEntity.course_no === course_no) {
+        hasFoundCartEntity = true;
+      }
+
+      var hasFoundMajorEntity = false;
+
+      angular.forEach(cartEntity.majors, function (majorEntity) {
+        if (majorEntity.code === major) {
+          addToClasses(classObj, majorEntity.classes);
+          hasFoundMajorEntity = true;
+          return false;
+        }
+      });
+
+      if (hasFoundMajorEntity) {
+        return false;
+      }
+      if (hasFoundCartEntity) {
+        addToMajors(classObj, cartEntity.majors);
+      }
     });
+
+    if (!hasFoundCartEntity) {
+      addToCart(classObj, classCart);
+    }
   };
 
-  $scope.classCart = convertStoredClassCart(storedContext.classCart);
+  var convertStoredClassCart = function (classCart) {
+    var newClassCart = [];
+
+    angular.forEach(classCart, function (cartEntity) {
+      var newEntity = angular.copy(cartEntity);
+
+      angular.forEach(newEntity.classes, function (classEntity) {
+        var deferredClass = $q.defer();
+
+        var classObj = Class.get({classNo: classEntity.class_no}, function () {
+          deferredClass.resolve(true);
+        });
+
+        classObj.placement_required = cartEntity.placement_required;
+        classObj.fixed = classEntity.fixed;
+
+        deferredClass.promise.then(function () {
+          addClassToClassCart(classObj, newClassCart);
+        });
+      });
+    });
+
+    return newClassCart;
+  };
+
+  var storedClassCart = angular.copy(storedContext.classCart);
+
+  $scope.classCart = convertStoredClassCart(storedClassCart);
   $scope.excludedTime = storedContext.excludedTime;
   $scope.options = storedContext.options;
+
+  var groupByCourseMajorGrade = function (classes) {
+    var newClasses = [];
+    var coursesMap = {};
+
+    angular.forEach(classes, function (c) {
+      var classObj = angular.copy(c);
+      var course_no = classObj.course_no;
+      var coursesMapEntity = coursesMap[course_no];
+      if (!coursesMapEntity) {
+        coursesMap[course_no] = {
+          course_no: course_no,
+          majorsMap: {}
+        };
+
+        coursesMapEntity = coursesMap[course_no];
+      }
+
+      var major = classObj.major;
+      var majorsMap = coursesMapEntity.majorsMap;
+      var majorsMapEntity = majorsMap[major];
+      if (!majorsMapEntity) {
+        majorsMap[major] = {
+          major: major,
+          gradesMap: {}
+        };
+
+        majorsMapEntity = majorsMap[major];
+      }
+
+      var grade = classObj.grade;
+      var gradesMap = majorsMapEntity.gradesMap;
+      var gradesMapEntity = gradesMap[grade];
+      if (!gradesMapEntity) {
+        var resultEntity = {
+          name: classObj.name,
+          course_no: course_no,
+          major: major,
+          grade: grade,
+          credit: classObj.credit,
+          classes: []
+        };
+
+        newClasses.push(resultEntity);
+
+        gradesMap[grade] = {
+          grade: grade,
+          resultEntity: resultEntity
+        };
+
+        gradesMapEntity = gradesMap[grade];
+      }
+
+      var subsetOfClass = {
+        instructor: classObj.instructor,
+        class_no: classObj.class_no,
+        time: classObj.time
+      };
+      gradesMapEntity.resultEntity.classes.push(subsetOfClass);
+    });
+
+    return newClasses;
+  };
+
+  $scope.search = function () {
+    var classes = Class.query(
+      {name: $scope.keyword},
+      function () {
+        $scope.classes = groupByCourseMajorGrade(classes);
+      }
+    );
+  };
 }]);
--- test/spec/controllers/Create.js
+++ test/spec/controllers/Create.js
@@ -9,21 +9,37 @@ describe('Controller: CreateCtrl', function () {
   var CreateCtrl,
     scope;
 
-  var MajorInfo = function () {
-    var majorsInfo = [
-      {name: '컴퓨터공학부', code: 'H3HADD'},
-      {name: '컴퓨터전공', code: 'H3HADDA'},
-      {name: '소프트웨어전공', code: 'H3HADDB'},
-      {name: '융합전자공학부', code: 'H3HADG'}
-    ];
+  var majorsInfo = [
+    {name: '컴퓨터공학부', code: 'H3HADD'},
+    {name: '컴퓨터전공', code: 'H3HADDA'},
+    {name: '소프트웨어전공', code: 'H3HADDB'},
+    {name: '융합전자공학부', code: 'H3HADG'}
+  ];
 
-    return {
-      getMajorsInfo: $q.when(majorsInfo)
+  var MajorInfo = {
+    getMajorsInfo: function () {return $q.when(majorsInfo);},
+    getNameByCode: function (code) {
+      var deferred = $q.defer();
+      var hasFoundName = false;
+
+      angular.forEach(majorsInfo, function (major) {
+        if (major.code === code) {
+          deferred.resolve(major.name);
+          hasFoundName = true;
+          return false;
+        }
+      });
+
+      if (!hasFoundName) {
+        deferred.reject('cannot find name for major code ' + code);
+      }
+
+      return deferred.promise;
     }
   };
 
-  var Class = function () {
-    this.query = function (parameters, success, error) {
+  var Class = {
+    query: function (parameters, success, error) {
       var result;
 
       if (parameters && parameters.name && parameters.name === '미분적분학') {
@@ -125,11 +141,13 @@ describe('Controller: CreateCtrl', function () {
         }
       }
 
-      success(result);
-      return $q.when(result);
-    };
+      $timeout(function () {
+        success(result);
+      }, 0);
+      return result;
+    },
 
-    this.get = function (parameters, success, error) {
+    get: function (parameters, success, error) {
       var classNo = parameters.classNo;
       var result;
 
@@ -137,14 +155,14 @@ describe('Controller: CreateCtrl', function () {
       case '13056':
         result = {
           class_no: '13056',
-          course_no: 'CSE406',
+          course_no: 'GEN253',
           name: '미분적분학2',
           major: 'H3HADD',
           time: [
             {start_time: 211, end_time: 213, room: 'H05-0103'},
             {start_time: 514, end_time: 516, room: 'H05-0103'}
           ],
-          score: 3.00,
+          credit: 3.00,
           grade: 1,
           instructor: null,
           category: '기초필수'
@@ -154,14 +172,14 @@ describe('Controller: CreateCtrl', function () {
       case '13057':
         result = {
           class_no: '13057',
-          course_no: 'CSE406',
+          course_no: 'GEN253',
           name: '미분적분학2',
           major: 'H3HADD',
           time: [
             {start_time: 211, end_time: 213, room: 'H05-0202'},
             {start_time: 514, end_time: 516, room: 'H05-0202'}
           ],
-          score: 3.00,
+          credit: 3.00,
           grade: 1,
           instructor: '신교일',
           category: '기초필수'
@@ -171,14 +189,14 @@ describe('Controller: CreateCtrl', function () {
       case '10455':
         result = {
           class_no: '10455',
-          course_no: 'CSE406',
+          course_no: 'GEN253',
           name: '미분적분학2',
           major: 'H3HADD',
           time: [
             {start_time: 211, end_time: 213, room: 'H05-0203'},
             {start_time: 514, end_time: 516, room: 'H05-0203'}
           ],
-          score: 3.00,
+          credit: 3.00,
           grade: 1,
           instructor: '평인수',
           category: '기초필수'
@@ -188,14 +206,14 @@ describe('Controller: CreateCtrl', function () {
       case '10303':
         result = {
           class_no: '10303',
-          course_no: 'CSE406',
+          course_no: 'GEN253',
           name: '미분적분학2',
           major: 'H3HADG',
           time: [
             {start_time: 114, end_time: 116, room: 'H05-0103'},
             {start_time: 417, end_time: 419, room: 'H05-0103'}
           ],
-          score: 3.00,
+          credit: 3.00,
           grade: 1,
           instructor: 'Benjamin Dieckman Willson',
           category: '기초필수'
@@ -205,14 +223,14 @@ describe('Controller: CreateCtrl', function () {
       case '10301':
         result = {
           class_no: '10301',
-          course_no: 'CSE406',
+          course_no: 'GEN253',
           name: '미분적분학2',
           major: 'H3HADG',
           time: [
             {start_time: 206, end_time: 208, room: 'H05-0103'},
             {start_time: 403, end_time: 405, room: 'H05-0103'}
           ],
-          score: 3.00,
+          credit: 3.00,
           grade: 1,
           instructor: '박영선',
           category: '기초필수'
@@ -222,14 +240,14 @@ describe('Controller: CreateCtrl', function () {
       case '10302':
         result = {
           class_no: '10302',
-          course_no: 'CSE406',
+          course_no: 'GEN253',
           name: '미분적분학2',
           major: 'H3HADG',
           time: [
             {start_time: 114, end_time: 116, room: 'H05-0102'},
             {start_time: 417, end_time: 419, room: 'H05-0102'}
           ],
-          score: 3.00,
+          credit: 3.00,
           grade: 1,
           instructor: '황신철',
           category: '기초필수'
@@ -237,21 +255,22 @@ describe('Controller: CreateCtrl', function () {
         break;
       }
 
-      success(result);
-      return $q.when(result);
+      $timeout(function () {
+        success(result);
+      }, 0);
+      return result;
     }
-
-    return this;
   };
 
   var storedContext = {
     classCart: [
       {
         placement_required: true,
-        course_no: 'CSE406',
+        course_no: 'GEN253',
         classes: [
-          {class_no: '10406', fixed: false},
-          {class_no: '10407', fixed: true}
+          {class_no: '13056', fixed: false},
+          {class_no: '13057', fixed: true},
+          {class_no: '10455', fixed: false}
         ]
       }
     ],
@@ -345,28 +364,32 @@ describe('Controller: CreateCtrl', function () {
     var expectedClassCart = [
       {
         placement_required: true,
-        course_no: 'CSE406',
-        name: '소프트웨어공학',
+        course_no: 'GEN253',
+        name: '미분적분학2',
         majors: [
           {
             code: 'H3HADD',
             name: '컴퓨터공학부',
-            grade: 3,
+            grade: 1,
             credit: 3.00,
             classes: [
-              {instructor: '유민수', class_no: '10406', fixed: false},
-              {instructor: '유인경', class_no: '10407', fixed: true}
+              {instructor: null, class_no: '13056', fixed: false},
+              {instructor: '신교일', class_no: '13057', fixed: true},
+              {instructor: '평인수', class_no: '10455', fixed: false}
             ]
           }
         ]
       }
     ];
+
+    $timeout.flush();
+
     expect(scope.classCart).toEqualData(expectedClassCart);
     expect(scope.excludedTime).toEqualData(storedContext.excludedTime);
     expect(scope.options).toEqualData(storedContext.options);
   });
 
-  it('should store context data', function () {
+  it('should delete a class entity from class cart and store the class cart into cookie', function () {
     var scopeClassCart = [
       {
         placement_required: true,
@@ -404,12 +427,47 @@ describe('Controller: CreateCtrl', function () {
       }
     ];
 
-    var expectedClassCart = [
+    var expectedScopeClassCart = [
+      {
+        placement_required: true,
+        course_no: 'CSE406',
+        name: '소프트웨어공학',
+        majors: [
+          {
+            code: 'H3HADD',
+            name: '컴퓨터공학부',
+            grade: 3,
+            credit: 3.00,
+            classes: [
+              {instructor: '유인경', class_no: '10407', fixed: true}
+            ]
+          }
+        ]
+      },
+      {
+        placement_required: true,
+        course_no: 'ITE316',
+        name: '데이터베이스시스템',
+        majors: [
+          {
+            code: 'H3HADD',
+            name: '컴퓨터공학부',
+            grade: 3,
+            credit: 3.00,
+            classes: [
+              {instructor: '김상욱', class_no: '10415', fixed: true},
+              {instructor: '김영훈', class_no: '10414', fixed: false}
+            ]
+          }
+        ]
+      }
+    ];
+
+    var expectedStoredClassCart = [
       {
         placement_required: true,
         course_no: 'CSE406',
         classes: [
-          {class_no: '10406', fixed: false},
           {class_no: '10407', fixed: true}
         ]
       },
@@ -426,11 +484,11 @@ describe('Controller: CreateCtrl', function () {
     spyOn(jQuery, 'cookie').andCallThrough();
 
     scope.classCart = scopeClassCart;
+    scope.deleteClass('10406');
 
-    $timeout.flush();
-
+    expect(scope.classCart).toBeEqual(expectedScopeClassCart);
     expect(jQuery.cookie)
-    .toHaveBeenCalledWith('context-create', expectedClassCart);
+    .toHaveBeenCalledWith('context-create', expectedStoredClassCart);
   });
 
   it('should load the list of classes from the server', function () {
